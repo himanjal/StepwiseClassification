0d7776624c195a7d3af036465a767c895c2beb98
himanjal <himanjalsharma1@gmail.com>
2018-03-19 22:13:50 -0700

    added looping

--- homework1_smile.py
+++ homework1_smile.py
@@ -31,7 +31,7 @@ def stepwiseRegression (trainingFaces, trainingLabels, testingFaces, testingLabe
 
 	predictors = []
 	bestAccuracy = []
-	for i in range(0,10):
+	for i in range(0,5):
 		bestAccuracy = 0
 		bestLocation = None
 		for r1 in range(0,24):
@@ -58,7 +58,7 @@ def stepwiseRegression (trainingFaces, trainingLabels, testingFaces, testingLabe
 	
 	r1, c1, r2, c2 = bestLocation
 
-	show = True
+	show = False
 	if show:
 		# Show an arbitrary test image in grayscale
 		im = testingFaces[0,:,:]
@@ -78,7 +78,22 @@ def stepwiseRegression (trainingFaces, trainingLabels, testingFaces, testingLabe
 
 	return predictors
 
-	
+
+def drawFeatures(predictors, testingFaces):
+	im = testingFaces[0,:,:]
+	fig,ax = plt.subplots(1)
+	ax.imshow(im, cmap='gray')
+	# Show r1,c1
+	for location in predictors:
+		r1, c1, r2, c2  = location
+		rect = patches.Rectangle((c1,r1),1,1,linewidth=2,edgecolor='r',facecolor='none')
+		ax.add_patch(rect)
+		# Show r2,c2
+		rect = patches.Rectangle((c2,r2),1,1,linewidth=2,edgecolor='b',facecolor='none')
+		ax.add_patch(rect)
+	# Display the merged result
+	plt.show()
+
 
     
 def loadData (which):
@@ -87,7 +102,57 @@ def loadData (which):
     labels = np.load("{}ingLabels.npy".format(which))
     return faces, labels
 
+def trainModel(trainingFaces,trainingLabels,testingFaces, testingLabels):
+	samples = [400,800,1200,1600,2000]
+
+	predictors = []
+	for sampleNo in samples:
+		log("Training Model with {} image samples.".format(sampleNo))
+		predictors = stepwiseRegression(trainingFaces[:sampleNo],trainingLabels[:sampleNo],testingFaces,testingLabels)
+
+		log("predictors selected{}".format(predictors))
+
+		log("Training Accuracy of model on Training Data")
+
+		accuracy = measureAccuracyOfPredictors(predictors,trainingFaces,trainingLabels)
+
+		log("Training accuracy with {} samples = {}".format(sampleNo, accuracy))
+		
+		accuracy = measureAccuracyOfPredictors(predictors,testingFaces,testingLabels)
+
+		log("Testing Accuracy with {} samples = {}".format(sampleNo, accuracy))
+
+	log("Final Accuracy of model = {}".format(accuracy))
+
+	log("Training with all the samples completed.")
+
+	log("Final Predictors selected:\n{}".format(predictors))
+
+	
+
+	drawFeatures(predictors,testingFaces)
+
+
+
+def log(text):
+	global logString
+	logString = logString + text + "\n"
+	print text
+
+
+logString = ""
+logging = True
 if __name__ == "__main__":
     testingFaces, testingLabels = loadData("test")
     trainingFaces, trainingLabels = loadData("train")
-    print stepwiseRegression(trainingFaces[:1000],trainingLabels[:1000],testingFaces,testingLabels)
+    #predictors = stepwiseRegression(trainingFaces[:2000],trainingLabels[:2000],testingFaces,testingLabels)
+    #predictors = [(20, 7, 17, 7), (13, 5, 11, 13), (20, 17, 16, 17), (12, 19, 12, 13), (10, 5, 14, 7), (13, 5, 15, 16), (22, 6, 16, 6), (12, 5, 11, 13), (18, 14, 14, 17), (3, 8, 2, 19)]
+    #print predictors
+    trainModel(trainingFaces,trainingLabels,testingFaces,testingLabels)
+    
+    if logging:
+    	with open("LogFile.txt", 'w') as fh:
+    		fh.write(logString)
+    		fh.close()
+    
+
